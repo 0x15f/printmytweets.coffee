9ead7cb6abc8a39b095a407fa6590a7079410dad
Jake Casto <jake@snaju.com>
2019-03-05 17:29:02 -0500

    Update homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,66 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <meta name="theme-color" content="#6f4e37">
 
-        <title>Laravel</title>
+        <title>Print My Tweets </title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="https://fonts.googleapis.com/css?family=PT+Mono:400,700" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/boxicons@1.9.1/css/boxicons.min.css" rel="stylesheet">
+        <link href="/jakescoffee.css" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div id="header-title"><h1><a href="#"><i class="bx bxs-coffee"></i><br>Print My Tweets</a></h1></div>
+        <header>
+            <table>
+                <tr>
+                    <td><a href="#about"><i class="bx bxs-coffee-alt"></i><br>About</a></td>
+                    <td><a href="#reviews"><i class="bx bxs-star"></i><br>Reviews</a></td>
+                    <td><a href="#buy"><i class="bx bxs-dollar-circle"></i><br>Buy</a></td>
+                </tr>
+            </table>
+        </header>
+        <main>
+            <div class="section" id="about">
+                <div class="grid one">
+                    <div class="grid-section">
+                        <center><img src="https://i.imgur.com/TAPm20X.jpg"></center>
+                    </div>
+                    <div class="grid-section">
+                        <h2>About</h2>
+                        <p>Retweeting is cool and all, but have you ever printed one of your favorite tweets onto a coffee mug? Come on! You know you want to look at this every morning.</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="section" id="reviews">
+                <h2>Reviews</h2>
+                <p><i>No customers yet :( You should be our first!</i></p>
+            </div>
+            <div class="section" id="buy">
+            <div class="grid two">
+                <div class="grid-section">
+                    <h2>Buy</h2>
+                    <p>What are you waiting for? You can get your favorite tweet printed on a coffee mug for only $12 (with free shipping included)!</p>
+                    <form action="https://account.1mb.site" method="POST">
+                        <input type="text" name="tweet" placeholder="Tweet Link">
+                        <button type="submit">Buy Now</button>
+                    </form>
+                </div>
+                <div class="grid-section">
+                    <center>
+                        <h2>Preview</h2>
+                        <img src="https://i.imgur.com/TAPm20X.jpg">
+                    </center>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <p>Jake's Coffee &copy; <span id="automatic_copyright_year"></span> - <a href="https://twitter.com/jakescoffee" target="_blank"><i class="bx bxl-twitter"></i> jakescoffee</a> - Designed by <a href="https://dalton.1mb.site" target="_blank">DE</a></p>
+        </footer>
+        <script>
+            document.getElementById('automatic_copyright_year').innerHTML = new Date().getFullYear();
+        </script>
     </body>
 </html>
