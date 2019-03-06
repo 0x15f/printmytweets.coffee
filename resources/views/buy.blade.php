599412695043bf261ec9b8bbd9441cc4d398261c
Jake Casto <jake@snaju.com>
2019-03-05 23:01:20 -0500

    Update buy.blade.php

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -52,6 +52,12 @@
                 font-size: 16px;
             }
 
+            .braintree-large-button:hover {
+                background-color: #e2dbd7;
+                font-family: "PT Mono", monospace;
+                font-size: 16px;
+            }
+
             .braintree-option:hover {
                 background-color: #e2dbd7;
                 font-family: "PT Mono", monospace;
