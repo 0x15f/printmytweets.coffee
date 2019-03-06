99e3f6d2ea9850b111c4fa516e6b23e3c2cb3d66
Jake Casto <jake@snaju.com>
2019-03-05 20:05:26 -0500

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -55,8 +55,8 @@ class ApiController extends Controller
     	$mockup_params = new MockupGenerationParameters;
     	$mockup_params->productId = 19;
     	$mockup_params->variantIds[] = 1320;
-    	$mockup_params->optionGroups[] = '';
-    	$mockup_params->addImageUrl('default', route('api.thumbnail', ['url' => $request->query('url')])/*, $position*/);
+    	// $mockup_params->optionGroups[] = '';
+    	$mockup_params->addImageUrl('default', route('api.thumbnail', ['url' => $request->query('url')]), $position);
 
     	$print_files = $mockup_client->createGenerationTaskAndWaitForResult($mockup_params);
 
