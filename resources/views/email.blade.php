ce16754d808d557559945b7826815da294127af2
Jake Casto <jake@snaju.com>
2019-03-06 12:17:23 -0500

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -3,7 +3,7 @@ Hey!
 
 Your order is being processed! You can view your order status below and we'll let you know once it ships.
 
-@component('mail::button', ['url' => route('order', ['id' => $info['id']])])
+@component('mail::button', ['url' => route('order', ['id' => $order['id']])])
 View Order
 @endcomponent
 
