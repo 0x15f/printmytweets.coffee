f9950b67c77588ff56509b57cefb6cff2239e2b7
Jake Casto <jake@snaju.com>
2019-03-05 23:16:52 -0500

    Update buy.blade.php

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -77,12 +77,8 @@
             }  
 
             .loader {
-                position: fixed;
-                left: 0px;
-                top: 0px;
                 width: 100%;
                 height: 100%;
-                z-index: 9999;
                 background-color: #e2dbd7;
             }
 
