847554a98abae0f036f2f1e2894cdfd6e63f2c08
Jake Casto <jake@snaju.com>
2019-03-06 12:40:49 -0500

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,8 +1,8 @@
 <p>
-	Hey,
+	Hey,<br><br>
 
-	Your order is being processed, your order id is <a href="{{ route('order', ['id' => $order['id']]) }}">#{{ $order['id'] }}</a>. We'll let you know when it ships!
+	Your order is being processed, your order id is <a href="{{ route('order', ['id' => $order['id']]) }}">#{{ $order['id'] }}</a>. We'll let you know when it ships!<br><br>
 
-	Thank you,
+	Thank you,<br>
 	Print My Tweets Team
 </p>
